eef1ac0576c1932d838cbd5eb167ded278cbe707
Lavanya <k.lavanya543@gmail.com>
2025-07-19 14:54:48 +0400

    first commit

---
+++ main.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow-ssh-http"
+  description = "Allow SSH (22) and HTTP (80)"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # HTTP access
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Python Flask default port
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "docker_host" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+  user_data              = file("install-docker.sh")
+  tags = {
+    Name = "docker-python-ec2"
+  }
+}
